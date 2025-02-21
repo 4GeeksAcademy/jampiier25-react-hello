b2a4d015c05f3ea40a7f7c42dea7baf2e30ebcbd
Jean Pierre Pluas <jampiier@gmail.com>
2025-02-21 21:11:40 +0000

    jampiier25

--- src/js/components/Carrusel.jsx
+++ src/js/components/Carrusel.jsx
@@ -6,13 +6,13 @@ const carrusel = () => {
         <div id="carouselExampleFade" class="carousel slide carousel-fade">
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg" class="d-block w-100" alt="..."></img>
+            <img src="https://i.blogs.es/a939eb/trabajo-en-remoto-en-verano/1366_2000.jpeg" class="d-block w-100" alt="..."></img>
           </div>
           <div className="carousel-item">
-            <img src="https://www.youtube.com/watch?v=YpoUzo-CflQ" class="d-block w-100" alt="..."></img>
+            <img src="https://cdn.businessinsider.es/sites/navi.axelspringer.es/public/media/image/2022/07/hombre-trabajando-playa-2763975.jpg?tf=3840x" class="d-block w-100" alt="..."></img>
           </div>
           <div className="carousel-item">
-            <img src="https://www.youtube.com/watch?v=YpoUzo-CflQ" class="d-block w-100" alt="..."></img>
+            <img src="https://nomadwebs.com/wp-content/uploads/2024/02/6521720.jpg" class="d-block w-100" alt="..."></img>
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
